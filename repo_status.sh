42ac39db33448969dc93b7242fa34d05f5ece2ad
Caleb Grant <geocoug@gmail.com>
2024-08-14 08:17:09 -0700

    cleanup
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- repo_status.sh
+++ repo_status.sh
@@ -5,25 +5,29 @@
 # Desc ....... Check the Git status for every subdirectory in a provided path. #
 ################################################################################
 
-TIMESLOT=`date +%Y_%m_%d.%H_%M`
+TIMESLOT=$(date +%Y_%m_%d.%H_%M)
 printf "[$TIMESLOT] START ...................... pull_repos\n"
 
-START_DIRECTORY=`pwd`
+START_DIRECTORY=$(pwd)
 printf "[$TIMESLOT] START_DIRECTORY ............ $START_DIRECTORY\n"
 printf "[$TIMESLOT] ............................\n"
 
-for SUBDIR in $START_DIRECTORY/*/;
-do
+# Use an array to handle directories properly
+shopt -s nullglob
+directories=("$START_DIRECTORY"/*/)
+
+for SUBDIR in "${directories[@]}"; do
     # Skip over directories containing the names "_archive" and "gist"
     # since those are not standard git repositories
     if [[ "$SUBDIR" =~ .*"_archive".* ]] || [[ "$SUBDIR" =~ .*"gist".* ]]; then
         continue
     fi
     printf "[$TIMESLOT] CHECKING ................... ${SUBDIR%*/}\n"
-    cd ${SUBDIR%*/}
-    STATUS=`git status`
-    # If the status returns as a clean tree then dont write anything to the console.
+    cd "${SUBDIR%*/}" || continue
+    STATUS=$(git status)
+    # If the status returns as a clean tree then don't write anything to the console.
     if grep -q "working tree clean" <<< "$STATUS"; then
+        cd ..
         continue
     # If the status returns anything besides clean, write the status to the console.
     else
@@ -31,8 +35,8 @@ do
     fi
     cd ..
     printf "[$TIMESLOT] ............................\n"
-done;
+done
 
-ENDTIME=`date +%Y_%m_%d.%H_%M`
+ENDTIME=$(date +%Y_%m_%d.%H_%M)
 printf "[$TIMESLOT] ............................\n"
 printf "[$TIMESLOT] END ........................ $ENDTIME\n\n"
